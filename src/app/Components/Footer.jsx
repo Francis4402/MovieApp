2036da210364875dff6da52466c6da1b362ba7a5
Francis <francism4402@gmail.com>
2024-09-26 06:00:50 +0600

    section updated

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -4,146 +4,147 @@ const Footer = () => {
   return (
 
 
-<div className='bg-slate-900'>
-<div className="mt-auto w-full container py-10 mx-auto md:px-0 px-5">
+<div className='bg-slate-900 mt-20'>
+  <div className="mt-auto w-full container py-10 mx-auto md:px-0 px-5">
 
-<div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-10">
-  <div className="col-span-full hidden lg:col-span-1 lg:block">
-    <a className="flex-none font-semibold text-xl text-white focus:outline-none focus:opacity-80" href="#" aria-label="Brand">Brand</a>
-    <p className="mt-3 text-xs sm:text-sm text-gray-400">
-      © 2024 Movie App.
-    </p>
+  <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-10">
+    <div className="col-span-full hidden lg:col-span-1 lg:block">
+      <a className="flex-none font-semibold text-xl text-white focus:outline-none focus:opacity-80" href="#" aria-label="Brand">Brand</a>
+      <p className="mt-3 text-xs sm:text-sm text-gray-400">
+        © 2024 Movie App.
+      </p>
+    </div>
+
+
+    <div>
+      <h4 className="text-xs font-semibold text-gray-300 uppercase">Product</h4>
+
+      <div className="mt-3 grid space-y-3 text-sm">
+        <p><a className="inline-flex gap-x-2 text-gray-300 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="#">Pricing</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-300 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="#">Changelog</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-300 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="#">Docs</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-300 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="#">Download</a></p>
+      </div>
+    </div>
+
+
+    <div>
+      <h4 className="text-xs font-semibold text-gray-900 uppercase">Company</h4>
+
+      <div className="mt-3 grid space-y-3 text-sm">
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">About us</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Blog</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Careers</a> <span className="inline text-blue-600">— We&apos;re hiring</span></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Customers</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Newsroom</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Sitemap</a></p>
+      </div>
+    </div>
+
+
+    <div>
+      <h4 className="text-xs font-semibold text-gray-900 uppercase">Resources</h4>
+
+      <div className="mt-3 grid space-y-3 text-sm">
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Community</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Help & Support</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">eBook</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">What&apos;s New</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Status</a></p>
+      </div>
+    </div>
+
+
+    <div>
+      <h4 className="text-xs font-semibold text-gray-900 uppercase">Developers</h4>
+
+      <div className="mt-3 grid space-y-3 text-sm">
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Api</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Status</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">GitHub</a> <span className="inline text-blue-600">— New</span></p>
+      </div>
+
+      <h4 className="mt-7 text-xs font-semibold text-gray-900 uppercase">Industries</h4>
+
+      <div className="mt-3 grid space-y-3 text-sm">
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Financial Services</a></p>
+        <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Education</a></p>
+      </div>
+    </div>
+
   </div>
 
 
-  <div>
-    <h4 className="text-xs font-semibold text-gray-300 uppercase">Product</h4>
+  <div className="pt-5 mt-5 border-t border-gray-200">
+    <div className="sm:flex sm:justify-between sm:items-center">
+      <div className="flex flex-wrap items-center gap-3">
 
-    <div className="mt-3 grid space-y-3 text-sm">
-      <p><a className="inline-flex gap-x-2 text-gray-300 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="#">Pricing</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-300 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="#">Changelog</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-300 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="#">Docs</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-300 hover:text-gray-500 focus:outline-none focus:text-gray-500" href="#">Download</a></p>
-    </div>
-  </div>
+        <div className="hs-dropdown [--placement:top-left] relative inline-flex">
 
-
-  <div>
-    <h4 className="text-xs font-semibold text-gray-900 uppercase">Company</h4>
-
-    <div className="mt-3 grid space-y-3 text-sm">
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">About us</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Blog</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Careers</a> <span className="inline text-blue-600">— We&apos;re hiring</span></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Customers</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Newsroom</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Sitemap</a></p>
-    </div>
-  </div>
-
-
-  <div>
-    <h4 className="text-xs font-semibold text-gray-900 uppercase">Resources</h4>
-
-    <div className="mt-3 grid space-y-3 text-sm">
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Community</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Help & Support</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">eBook</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">What&apos;s New</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Status</a></p>
-    </div>
-  </div>
-
-
-  <div>
-    <h4 className="text-xs font-semibold text-gray-900 uppercase">Developers</h4>
-
-    <div className="mt-3 grid space-y-3 text-sm">
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Api</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Status</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">GitHub</a> <span className="inline text-blue-600">— New</span></p>
-    </div>
-
-    <h4 className="mt-7 text-xs font-semibold text-gray-900 uppercase">Industries</h4>
-
-    <div className="mt-3 grid space-y-3 text-sm">
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Financial Services</a></p>
-      <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Education</a></p>
-    </div>
-  </div>
-
-</div>
-
-
-<div className="pt-5 mt-5 border-t border-gray-200">
-  <div className="sm:flex sm:justify-between sm:items-center">
-    <div className="flex flex-wrap items-center gap-3">
-
-      <div className="hs-dropdown [--placement:top-left] relative inline-flex">
-
-        <button id="hs-footer-language-dropdown" type="button" className="hs-dropdown-toggle py-2 px-3 inline-flex items-center gap-x-2 text-sm rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
-          
-          English (US)
-          
-        </button>
-
-        <div className="hs-dropdown-menu w-40 transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden z-10 bg-white shadow-md rounded-lg p-2" role="menu" aria-orientation="vertical" aria-labelledby="hs-footer-language-dropdown">
-          <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+          <button id="hs-footer-language-dropdown" type="button" className="hs-dropdown-toggle py-2 px-3 inline-flex items-center gap-x-2 text-sm rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none" aria-haspopup="menu" aria-expanded="false" aria-label="Dropdown">
             
             English (US)
-          </a>
-          <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
             
-            Deutsch
-          </a>
-          <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
-            
-            Dansk
-          </a>
-          <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
-            
-            Italiano
-          </a>
-          <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
-            
-          </a>
+          </button>
+
+          <div className="hs-dropdown-menu w-40 transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden z-10 bg-white shadow-md rounded-lg p-2" role="menu" aria-orientation="vertical" aria-labelledby="hs-footer-language-dropdown">
+            <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+              
+              English (US)
+            </a>
+            <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+              
+              Deutsch
+            </a>
+            <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+              
+              Dansk
+            </a>
+            <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+              
+              Italiano
+            </a>
+            <a className="flex items-center gap-x-2 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100" href="#">
+              
+            </a>
+          </div>
+        </div>
+
+
+        <div className="space-x-4 text-sm">
+          <a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Terms</a>
+          <a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Privacy</a>
+          <a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Status</a>
         </div>
       </div>
 
-
-      <div className="space-x-4 text-sm">
-        <a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Terms</a>
-        <a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Privacy</a>
-        <a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">Status</a>
-      </div>
-    </div>
-
-    <div className="flex flex-wrap justify-between items-center gap-3">
-      <div className="mt-3 sm:hidden">
-        <a className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80" href="#" aria-label="Brand">Brand</a>
-        <p className="mt-1 text-xs sm:text-sm text-gray-600">
-          © 2024 Preline Labs.
-        </p>
-      </div>
+      <div className="flex flex-wrap justify-between items-center gap-3">
+        <div className="mt-3 sm:hidden">
+          <a className="flex-none font-semibold text-xl text-black focus:outline-none focus:opacity-80" href="#" aria-label="Brand">Brand</a>
+          <p className="mt-1 text-xs sm:text-sm text-gray-600">
+            © 2024 Preline Labs.
+          </p>
+        </div>
 
 
-      <div className="space-x-4">
-        <a className="inline-block text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">
-          
-        </a>
-        <a className="inline-block text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">
-          
-        </a>
-        <a className="inline-block text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">
-          
-        </a>
+        <div className="space-x-4">
+          <a className="inline-block text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">
+            
+          </a>
+          <a className="inline-block text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">
+            
+          </a>
+          <a className="inline-block text-gray-500 hover:text-gray-800 focus:outline-none focus:text-gray-800" href="#">
+            
+          </a>
+        </div>
+
       </div>
 
     </div>
-
   </div>
-</div>
-</div>
+  </div>
+  
 </div>
 
 
--- src/app/Components/TopMovieLists.jsx
+++ src/app/Components/TopMovieLists.jsx
@@ -1,11 +1,13 @@
+import { CardImages } from '@/Index'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { FaPlus } from 'react-icons/fa'
 
 const TopMovieLists = () => {
   return (
-    <div className='flex justify-center gap-10 items-start'>
-        <div className='flex flex-col bg-gray-600/30 w-[40rem] p-5'>
+    <div className='flex lg:flex-row flex-col justify-center gap-10 items-start'>
+        <div className='flex flex-col bg-gray-600/30 lg:w-[40rem] w-full lg:p-5 px-10 py-5'>
             <div className='flex justify-between items-center text-white font-sans'>
                 <h2 className='text-2xl font-semibold'>Top 9 this Week</h2>
 
@@ -32,8 +34,40 @@ const TopMovieLists = () => {
             
             <hr className='mb-5 border-gray-700' />
 
-            <div className='flex gap-5'>
-                <Image src={'/cards/cardimg6.png'} alt="i" width={200} height={200}/>
+            <div className='flex gap-10'>
+                <Image src={'/cards/cardimg6.png'} alt="i" width={150} height={150}/>
+
+                <div className='flex flex-col gap-5'>
+                    <div className='flex flex-col gap-2'>
+                        <p className='text-gray-400 text-sm'>2024</p>
+                        <h3 className='text-white text-xl font-semibold'>Last Breath</h3>
+                    </div>
+                    <p className='text-gray-400'>Aliens Invated Our Planed People all the humans fighting for their pitiful lifes</p>
+                    
+                    <div className='flex gap-5'>
+                        <button className='bg-blue-400 duration-200 hover:bg-blue-600 text-white px-5 py-2 rounded-md'>Watch Now</button>
+                        <button className='flex items-center hover:bg-white hover:text-black px-5 py-2 rounded-md duration-200 gap-1 text-xs text-white font-semibold'><FaPlus size={9} />PLAYLIST</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className='grid grid-cols-2'>
+                {
+                    CardImages.slice(0, 8).map(items => <div key={items.src} className=' border-gray-700'>
+                        <div className='flex gap-5'>
+                            <Image src={items.src} alt="i" width={80} height={80} />
+                            <div className='flex flex-col gap-2'>
+                                <p className='text-gray-400 text-sm'>{items.year}</p>
+                                <h3 className='text-white font-semibold'>{items.title}</h3>
+                                <div className='flex gap-5'>
+                                    <p className='text-gray-400 text-sm'>{items.type}</p>
+                                    <p className='text-gray-400 text-sm'>{items.rating}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <hr className='border-gray-700 my-5 w-[80%]'/>
+                    </div>)
+                }
             </div>
         </div>
         
